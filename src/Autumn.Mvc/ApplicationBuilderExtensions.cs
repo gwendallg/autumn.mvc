abcf97d107346f782d0d7b5e69b70f20d04a7282
gwendallg <gwendall.garnier.fr@gmail.com>
2017-12-09 08:36:53 +0100

    prepare version

---
+++ src/Autumn.Mvc/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Autumn.Mvc.Configurations;
+using Microsoft.AspNetCore.Builder;
+
+namespace Autumn.Mvc
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static AutumnSettings GetAutumnSettings(this IApplicationBuilder app)
+        {
+            return (AutumnSettings) app.ApplicationServices.GetService(typeof(AutumnSettings));
+        }
+    }
+}
